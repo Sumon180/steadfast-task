f0418c27133fd5dc5c62dd2ac25afa3e16aada72
Sumon Hossain <sumonengr.96@gmail.com>
2025-06-30 19:31:21 +0600

    RelatedProducts

--- app/(home)/product/[slug]/components/RelatedProducts.tsx
+++ app/(home)/product/[slug]/components/RelatedProducts.tsx
@@ -23,16 +23,19 @@ export default function RelatedProducts({ categoryId, currentSlug }: Props) {
 
   return (
     <div className="md:px-4 px-3 py-3">
-      <div className="max-w-[1280px] mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
-        {related.length === 0 ? (
-          <p className="text-center text-gray-500 col-span-full">
-            No related products found.
-          </p>
-        ) : (
-          related.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))
-        )}
+      <div className="content_wrapper">
+        <p className="text-2xl font-semibold mb-2">Related Products</p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+          {related.length === 0 ? (
+            <p className="text-center text-gray-500 col-span-full">
+              No related products found.
+            </p>
+          ) : (
+            related.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))
+          )}
+        </div>
       </div>
     </div>
   );
